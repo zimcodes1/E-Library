334185345f3ca73956dd75c55029f34ba3714695
Azimeh <azimehobadiah1@gmail.com>
2025-12-28 23:26:38 +0100

    feat: add Footer component with navigation links and app download section

---
+++ client/src/components/Footer.tsx
@@ -0,0 +1,43 @@
+function Footer() {
+    return (
+        <div className="w-full px-20 py-5 bg-blue-100 h-80 flex flex-col justify-evenly items-start">
+            <div className="w-full h-3/4 flex justify-evenly items-start">
+                <span className="w-1/4 flex flex-col items-start justify-center">
+                    <h3 className="text-lg font-bold">Company</h3>
+                    <p className="text-sm mt-2">About Us</p>
+                    <p className="text-sm mt-2">Careers</p>
+                    <p className="text-sm mt-2">Our Team</p>
+                </span>
+                <span className="w-1/4 flex flex-col items-start justify-center">
+                    <h3 className="text-lg font-bold">Resources</h3>
+                    <p className="text-sm mt-2">Blog</p>
+                    <p className="text-sm mt-2">Help Center</p>
+                    <p className="text-sm mt-2">Support</p>
+                </span>
+                <span className="w-1/4 flex flex-col items-start justify-center">
+                    <h3 className="text-lg font-bold">Legal</h3>
+                    <p className="text-sm mt-2">Privacy Policy</p>
+                    <p className="text-sm mt-2">Terms of Service</p>
+                </span>
+                <span className="w-1/4 flex flex-col items-start justify-center">
+                    <h3 className="text-lg font-bold">Contact</h3>
+                    <p className="text-sm mt-2"><i className="fa fa-envelope text-red-500"></i> Email Us</p>
+                    <p className="text-sm mt-2"><i className="fa fa-phone"></i>Call Us</p>
+                    <p className="text-sm mt-2"><i className="fa fab fa-facebook text-blue-500"></i>acebook</p>
+                </span>
+            </div>
+
+            {/* Download App Section */}
+            <div className="w-full h-1/4 flex justify-center items-center">
+                <span>Get our free apps:</span>
+                <span className="flex flex-col mx-10">
+                    <img src="/images/playstore.png" className="w-auto h-10" alt="Google Play Store" />
+                    <img src="/images/appstore.png" className="w-auto h-10" alt="Apple App Store" />
+                </span>
+            </div>
+            <hr className="border border-gray-500 w-full mt-5" />
+        </div>
+    )
+}
+
+export default Footer
--- client/src/pages/LandingPage.tsx
+++ client/src/pages/LandingPage.tsx
@@ -3,23 +3,29 @@ import AllInOne from "../components/AllInOne";
 import Reviews from "../components/Reviews";
 import BooksBanner from "../components/BooksBanner";
 import Button from "../components/ui/Button";
+import Footer from "../components/Footer";
 
 function LandingPage() {
     return (
-        <div className="w-full h-fit px-20 bg-orange-50 pt-5 -z-10">
-            {/* Hero Section */}
-            <HeroSection></HeroSection>
-            {/* All In One Section */}
-            <AllInOne></AllInOne>
-            {/* Reviews Section */}
-            <Reviews></Reviews>
-            {/* Books Banner Section */}
-            <BooksBanner></BooksBanner>
-            {/* Call to Action Section */}
-            <span className="flex">
-                <Button text='30 days free trial' styles="mx-auto"></Button>
-            </span>
-        </div>)
+        <>
+            <div className="w-full h-fit px-20 bg-orange-50 pt-5 -z-10">
+                {/* Hero Section */}
+                <HeroSection></HeroSection>
+                {/* All In One Section */}
+                <AllInOne></AllInOne>
+                {/* Reviews Section */}
+                <Reviews></Reviews>
+                {/* Books Banner Section */}
+                <BooksBanner></BooksBanner>
+                {/* Call to Action Section */}
+                <span className="flex">
+                    <Button text='30 days free trial' styles="mx-auto mb-10"></Button>
+                </span>
+            </div>
+            {/* Footer Section */}
+            <Footer></Footer>
+        </>
+    )
 }
 
 export default LandingPage;
