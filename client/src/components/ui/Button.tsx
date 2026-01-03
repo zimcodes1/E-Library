0de61a5e4a4d2c26160b25e850e5db718b5524a5
Azimeh <azimehobadiah1@gmail.com>
2026-01-03 19:06:07 +0100

    Add transitioned hover animations to Buttons and Categories cards

--- client/src/components/ui/Button.tsx
+++ client/src/components/ui/Button.tsx
@@ -4,7 +4,7 @@ function Button({text, styles}: {text: string, styles?: string}) {
     return(
         <button className={` text-gray-800 px-5 cursor-pointer py-2.5 
             rounded-[80px] text-sm max-sm:text-sm font-medium bg-gray-50
-            hover:shadow-md transition duration-300 hover:shadow-[#4040405f] ${styles || ''}`}>{text}</button>
+            hover:shadow-md transition duration-500 hover:bg-gray-950 border-gray-50 border-2 hover:text-gray-50 ${styles || ''}`}>{text}</button>
     )
 }
 
