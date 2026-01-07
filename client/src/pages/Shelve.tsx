e2dafed9f7556d3333ced75341fdc053aed477b7
Azimeh <azimehobadiah1@gmail.com>
2026-01-07 16:27:58 +0100

    feat: make shelve page responsive for small screen devices

--- client/src/pages/Shelve.tsx
+++ client/src/pages/Shelve.tsx
@@ -7,22 +7,22 @@ import { useEffect } from "react"
 const Shelve = () => {
     useEffect(()=>{document.title = 'Your Shelve | Libronet'}, [])
     return (
-        <div className="w-full flex justify-end items-center bgImage min-h-screen pb-10">
+        <div className="w-full flex justify-end items-center bgImage min-h-screen pb-10 max-sm:pb-25">
             {/* Side Navigation Menu */}
             <SideMenu />
-            <div className="w-6/7 h-screen flex flex-col px-10 pt-5 relative">
+            <div className="w-6/7 max-sm:w-full min-h-screen flex flex-col px-10 max-sm:px-3 pt-5 relative">
                 {/* Topbar component for Search Feature, Language Switch ... */}
                 <TopBar />
                 {/* Main Contents */}
-                <div className="w-full h-full flex mt-15 flex-col">
-                    <span className="flex justify-start items-center">
-                        <h2 className="text-lg font-semibold text-gray-300">Your Shelve</h2>
+                <div className="w-full h-full flex mt-15 max-sm:mt-3 flex-col">
+                    <span className="flex justify-start max-sm:justify-center items-center">
+                        <h2 className="text-lg max-sm:font-normal font-semibold text-gray-300">Your Shelve</h2>
                         <span className="flex h-9 min-w-25 px-2 ml-10 rounded-4xl bg-[#4857605a] justify-center items-center border border-gray-700 shadow cursor-pointer">
                             <CustomSelect defaultValue="All Books" options={[{value:'Favourites', label:'Favourites'}, {value:'Bookmarks', label:'Bookmark'},{value:'Downloads'}]}></CustomSelect>
                         </span>
                     </span>
                     {/* Shelve Container */}
-                    <div className="w-full h-fit flex justify-start items-center flex-wrap mt-5 text-sm gap-10">
+                    <div className="w-full h-fit flex justify-start items-center flex-wrap max-sm:flex-col mt-5 text-sm gap-10 max-sm:gap-2">
                         <ShelveItem></ShelveItem>
                         <ShelveItem></ShelveItem>
                         <ShelveItem></ShelveItem>
