e5a37742f828a0dc3f7e56c186b875a0cb0515b8
Azimeh <azimehobadiah1@gmail.com>
2026-02-26 11:40:40 +0100

    configure django for  vercel deployment

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -31,6 +31,11 @@ DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*').split(',')
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://libronet.vercel.app",
+    "https://*.vercel.app",
+]
+
 
 # Application definition
 
@@ -128,6 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
