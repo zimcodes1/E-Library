82aa1f104d7ba84bf91ce8748654522dd504ee82
Azimeh <azimehobadiah1@gmail.com>
2026-03-06 05:58:42 +0100

    minor responsiveness update

--- client/src/pages/Shelve.tsx
+++ client/src/pages/Shelve.tsx
@@ -58,9 +58,9 @@ const Shelve = () => {
                 <TopBar />
                 {/* Main Contents */}
                 <div className="w-full h-full flex mt-15 max-sm:mt-3 flex-col">
-                    <span className="flex justify-start max-sm:justify-center items-center">
-                        <h2 className="text-lg max-sm:font-normal font-semibold text-gray-300">Your Shelve ({shelveItems.length})</h2>
-                        <span className="flex h-9 min-w-25 px-2 ml-10 rounded-4xl bg-[#4857605a] justify-center items-center border border-gray-700 shadow cursor-pointer">
+                    <span className="flex justify-start max-sm:justify-between max-sm:px-5 items-center">
+                        <h2 className="text-lg max-sm:text-sm max-sm:font-normal font-semibold text-gray-300">Your Shelve ({shelveItems.length})</h2>
+                        <span className="flex max-sm:w-30 h-9 min-w-25 px-2 ml-10 rounded-4xl bg-[#4857605a] justify-center items-center border border-gray-700 shadow cursor-pointer">
                             <CustomSelect 
                                 defaultValue="All Books" 
                                 options={[
